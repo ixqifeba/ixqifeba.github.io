c88ec3a1fde7f1c3581cac12284da833a0681ba7
UG <uttie@callisto.bergtold>
2018-11-02 22:45:34 -0700

    update conf to test theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,12 +22,7 @@ AUTHOR_FEED_RSS = None
 
 # Theme
 THEME = "themes/pure-single"
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+TAGLINE = "taaaaglineee.ee"
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -37,3 +32,7 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Plugins
+PLUGIN_PATHS = ['./plugins'] #,  'themes/voce/plugins']
+PLUGINS = ['assets']
