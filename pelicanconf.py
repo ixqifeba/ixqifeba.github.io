870662226e2e0f086b8f0eb7cdcf4b7ec2871263
UG <uttie@callisto.bergtold>
2018-11-04 15:33:54 -0800

    update SITE_URL for dev and publish modes

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'ixqifeba'
 SITENAME = 'wasurerareru'
-SITEURL = ''
+SITEURL = 'http://localhost:8000/'
 
 PATH = 'content'
 PAGE_PATHS = ['pages']
