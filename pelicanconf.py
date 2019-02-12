3ec254cc4991e742e8325ef4d41abe29c820fab0
ae <ae>
2019-02-11 21:38:49 -0800

    Remove a bunch of unnecessary pages. Render a draft page.

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,14 +23,33 @@ AUTHOR_FEED_RSS = None
 
 # Look
 THEME = "themes/pure-single"
-HIDE_CATEGORIES = True
-POSTS_LIST = None #options 'tags', 'categories', None
+#HIDE_CATEGORIES = True
 COVER_IMG_URL = '/images/eberhard-grossgasteiger-545770-unsplash.jpg'
 TAGLINE = "documented moltings"
+POSTS_LIST = None  #options 'tags', 'categories', None
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
 MENUITEMS = [
-    ('About', './pages/about.html')
+    ('About', '/pages/about.html'),
+    ('WIP', '/pages/drafts.html')
 ]
-DISPLAY_PAGES_ON_MENU = True
+
+# What to render and how
+ARTICLE_URL = 'done/{slug}/'
+ARTICLE_SAVE_AS = 'done/{slug}/index.html'
+DRAFT_URL = 'wip/{slug}.html'
+DRAFT_SAVE_AS = 'wip/{slug}.html'
+TAG_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+ARCHIVES_SAVE_AS = ''
+# render drafts listing
+TEMPLATE_PAGES = {
+    'drafts.html' : 'pages/drafts.html'
+}
+
+DISQUS_ON_PAGES = False
 DEFAULT_PAGINATION = False
 TYPOGRIFY = True
 ARTICLE_ORDER_BY = 'reversed-date'
